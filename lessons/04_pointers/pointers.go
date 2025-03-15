4c22b835985af21e7333de0b23e29f827fc0cb51
Angael <krzysztofwidacki@gmail.com>
2025-03-15 01:00:36 +0100

    Add Person struct and demonstrate pointer usage in pointers lesson

--- lessons/04_pointers/pointers.go
+++ lessons/04_pointers/pointers.go
@@ -1,5 +1,25 @@
 package pointers
 
-func Run() {
+import "fmt"
 
+type Person struct {
+	Name string
+	Age  int
+}
+
+func Run() {
+	var p1 Person = Person{Name: "John", Age: 30}
+	p2 := &p1
+
+	p2.Name = "Doe"
+	p2.Age = 40
+
+	fmt.Println("Person 1: ", p1)
+	fmt.Println("Person 2: ", *p2)
+
+	p1.Name = "Alice"
+	p1.Age = 25
+
+	fmt.Println("Person 1: ", p1)
+	fmt.Println("Person 2: ", *p2)
 }
